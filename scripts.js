900f7e40afcebd7d1ce1ac2b0a98635020e5c69e
Agustin Moras <a.morascastro@gmail.com>
2021-07-19 14:16:55 -0300

    Erased Script

--- scripts.js
+++ scripts.js
@@ -1,21 +1,4 @@
-function showP(idProyecto){
-    
-    var proyecto = document.getElementById("idProyecto");
-    var descripcion = null;
 
-    for (let i = 0; i < proyecto.children.length; i++){
-        if(proyecto.proyecto.children.className == 'descripcion-proyecto'){
-            descripcion = proyecto.proyecto.children[i];
-            break;
-        }
-    }
-
-    if (descripcion.style.display === 'none'){
-        descripcion.style.display = "block";
-    } else {
-        descripcion.style.display = "none";
-    }
-}
 
 
 

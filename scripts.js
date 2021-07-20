bf483e2fbd87eaff7463f483c5d8ec50b7d2e6ea
Agustin Moras <a.morascastro@gmail.com>
2021-07-19 21:11:36 -0300

    Finished content and style

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
+function randomize(elemId) {
+    document.getElementById(elemId).style.color = randomColors();
+  }
 
-
-
+  function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
 
